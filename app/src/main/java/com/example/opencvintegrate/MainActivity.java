a3754ff064bccf703398c8b13112cfbba4b86284
Armando Salazar <armandosalazar351@gmail.com>
2023-01-18 00:09:55 -0600

    OpenCV sdk Works

---
+++ app/src/main/java/com/example/opencvintegrate/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.opencvintegrate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (OpenCVLoader.initDebug()) Log.d("LOADED", "success");
+        else Log.d("LOADED", "error");
+    }
+}
