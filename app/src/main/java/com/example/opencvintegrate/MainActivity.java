e61bf5dfbb829dcd32fb77bb07ae4f55e462851b
Armando Salazar <armandosalazar351@gmail.com>
2023-01-24 01:08:46 -0600

    OpenCV Camera Works

--- app/src/main/java/com/example/opencvintegrate/MainActivity.java
+++ app/src/main/java/com/example/opencvintegrate/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.opencvintegrate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 
 import org.opencv.android.OpenCVLoader;
 
@@ -15,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (OpenCVLoader.initDebug()) Log.d("LOADED", "success");
         else Log.d("LOADED", "error");
+
+        Button buttonCamera = findViewById(R.id.buttonCamera);
+        buttonCamera.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, CameraActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP)));
     }
 }
